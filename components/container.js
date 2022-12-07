cd7813c000660b5b1cd37e6c5da3e55dd4c437a2
Xavier Maillou <axrevi@gmail.com>
2022-12-07 01:25:26 +0100

    compiled

--- components/container.js
+++ components/container.js
@@ -3,6 +3,6 @@ import { fusionStyles } from "../lib";
 import { basicStyle } from "../lib/style";
 const Container = (props) => {
     const styles = fusionStyles(props.sx, props.spacing);
-    return (React.createElement("div", { style: Object.assign(Object.assign({}, styles), basicStyle) }));
+    return (React.createElement("div", { style: Object.assign(Object.assign({}, basicStyle), styles) }));
 };
 export default Container;
